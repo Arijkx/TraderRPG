f8b0d605cc12b72a801206aa024d92fdbc9cc427
Arijkx <85964300+Arijkx@users.noreply.github.com>
2026-02-15 17:20:12 +0100

    update 2.1

--- js/main.js
+++ js/main.js
@@ -12,4 +12,21 @@
     toggleChartExpand: G.toggleChartExpand,
   };
   G.init();
+
+  var scrollBtn = document.getElementById("scroll-to-top");
+  if (scrollBtn) {
+    var scrollThreshold = 200;
+    function updateScrollButton() {
+      if (window.scrollY > scrollThreshold) {
+        scrollBtn.classList.add("visible");
+      } else {
+        scrollBtn.classList.remove("visible");
+      }
+    }
+    window.addEventListener("scroll", updateScrollButton, { passive: true });
+    updateScrollButton();
+    scrollBtn.addEventListener("click", function () {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  }
 })();
